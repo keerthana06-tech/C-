83c617dac7f3063841c11fcef73a52b3f49c26a5
Keerthana c <keerthanagitify@gmail.com>
2025-08-13 21:11:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        reverse obj = new reverse();
+        Console.WriteLine("enter number:");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Reversed number = " + obj.get_reverse(number));
+    }
+}
