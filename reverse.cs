9e546df6ac1a82914b4fe6a3797b60feeb16f197
Keerthana c <keerthanagitify@gmail.com>
2025-08-13 21:11:44 +0530

    Add files via upload

---
+++ reverse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class reverse
+{
+    public int get_reverse(int number)
+    {
+        int rev = 0;
+        while (number != 0)
+        {
+            rev = rev * 10 + number % 10;
+            number /= 10;
+        }
+        return rev;
+    }
+}
