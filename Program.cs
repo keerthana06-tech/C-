187f4ce225f6442a8b12381d38281538e1ffe6ec
Keerthana c <keerthanagitify@gmail.com>
2025-09-04 13:58:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿using System;
+﻿namespace class_object_demo;
 
 class Program
 {
     static void Main(string[] args)
     {
-        BankAccount account = new BankAccount(5000);
-
-        try
-        {
-            Console.WriteLine("Enter amount to withdraw: ");
-            double amount = Convert.ToDouble(Console.ReadLine());
-
-            account.Withdraw(amount);
-        }
-        catch (InsufficientFundsException ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
-
-        Console.WriteLine("Final Balance: " + account.GetBalance());
+        Console.WriteLine("Hello, World!");
     }
 }
