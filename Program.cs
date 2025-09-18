8b4b39f6c9ab5c44e3ef9ce519ba1bacface5470
Keerthana c <keerthanagitify@gmail.com>
2025-09-18 14:18:47 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace invalid_mark_exception
+﻿namespace Calculator
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.Write("Enter first number: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter second number: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter operation (+, -, *, /): ");
+                char op = Convert.ToChar(Console.ReadLine());
+
+                Calculator calc = new Calculator();
+                int result = calc.Calculate(a, b, op);
+
+                Console.WriteLine("Result = " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine(" Error: Cannot divide by zero!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Error: Please enter valid numbers!");
+            }
+            finally
+            {
+                Console.WriteLine(" Calculation attempt complete.");
+            }
         }
     }
 }
---
+++ calculate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+
+
+    public class Calculator
+    {
+        public int Calculate(int a, int b, char op)
+        {
+            int result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+
+            return result;
+        }
+    }
+
+}
